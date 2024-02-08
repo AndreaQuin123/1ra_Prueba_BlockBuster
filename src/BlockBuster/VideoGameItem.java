941d91fc70ff4a141bf095b403ae828da8e165f4
hectorsabac <147878929+hectorsabac@users.noreply.github.com>
2024-02-08 11:48:45 -0600

    Clase VideoGameItem agregada
    Agrega a los archivos la clase VideoGameItem

---
+++ src/BlockBuster/VideoGameItem.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba1p2_hectorflores;
+
+public class VideoGameItem extends BlockBusterItem{
+    String nombreConsola;
+    
+    public enum consolas{
+        PLAYSTATION, XBOX, WII;
+    }
+
+    public VideoGameItem(String nombreConsola, int codigo, String nombre, double renta) {
+        super(codigo, nombre, 30);
+        this.nombreConsola = nombreConsola;
+    }
+
+    public String getNombreConsola() {
+        return nombreConsola;
+    }
+
+    public void setNombreConsola(String nombreConsola) {
+        this.nombreConsola = nombreConsola;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + "Game";
+    }
+
+    @Override
+    public double pagoRenta(int dias) {
+        return dias * renta;
+    }
+    
+    
+}
